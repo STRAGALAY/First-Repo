5273cbaafbfc804e70dda802446085efce39f9de
Fred <90625393+STRAGALAY@users.noreply.github.com>
2021-09-17 09:04:18 -0400

    Update ASD.py

--- ASD.py
+++ ASD.py
@@ -1,1 +1,18 @@
 print('testing')
+print('nice')
+
+# Visual Studio Code has `Ctrl Alt Down/Up` to duplicate lines up or down
+
+# here's a triangle
+
+print(
+    [
+        [
+            [
+                [
+                    []
+                ]
+            ]
+        ]
+    ]
+)
